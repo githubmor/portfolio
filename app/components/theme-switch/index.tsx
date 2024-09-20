a2c528a36975fa6911e93818797d3a2b0d76973b
githubmor <pc2man@gmail.com>
2024-09-20 12:48:43 +0330

    install daisyui

---
+++ app/components/theme-switch/index.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    // Check for stored theme in localStorage or default to 'light'
+    const storedTheme = localStorage.getItem("theme") || "light";
+    setTheme(storedTheme);
+    document.documentElement.setAttribute("data-theme", storedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className="btn">
+      Switch to {theme === "light" ? "dark" : "light"} mode
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./node_modules/daisyui/dist/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        grey: {
+          DEFAULT:"var(--grey)",
+          50:"var(--grey-50)",
+          100:"var(--grey-100)",
+          200:"var(--grey-200)",
+          300:"var(--grey-300)",
+          400:"var(--grey-400)",
+          500:"var(--grey-500)",
+          600:"var(--grey-600)",
+          700:"var(--grey-700)",
+          800:"var(--grey-800)",
+          900:"var(--grey-900)",
+          950:"var(--grey-950)",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {themes:false},
 };
 export default config;
