31e12bec47f6ad607f6e6be76436e1b96c46b134
githubmor <m.darzi@pooyasamaneh.net>
2024-09-28 17:01:13 +0330

    upgade

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 
 import Header from "./components/header";
-import ThemeSwitcher from "./components/theme-switch";
 
 export default function Home() {
    
   return (
     <div className="w-full h-full">
       <Header />
-      <main className=" flex gap-4">
-        <ThemeSwitcher />
-      </main>
+      <main className=" flex gap-4"></main>
       <footer className=""></footer>
     </div>
   );
