2aff017fd7e131e4c3406d211b75fed1bbfc4b57
githubmor <m.darzi@pooyasamaneh.net>
2024-11-13 11:20:03 +0330

    add link smoth scroll

--- app/(sections)/header/menus/index.tsx
+++ app/(sections)/header/menus/index.tsx
@@ -9,10 +9,10 @@ function Menus({ className = "" }: Props) {
     <div
       className={`flex flex-col md:flex md:flex-row gap-4 md:gap-6 border-b border-gray-100 md:border-0 p-4 md:p-0 ${className}`}
     >
-      <Links text="About" isMenu href="/about" />
-      <Links text="Work" isMenu href="/work" />
-      <Links text="Testimonials" isMenu href="/testi" />
-      <Links text="Contact" isMenu href="/contact" />
+      <Links text="About" isMenu href="/#about" />
+      <Links text="Work" isMenu href="/#work" />
+      <Links text="Testimonials" isMenu href="/#testimonials" />
+      <Links text="Contact" isMenu href="/#contact-me" />
     </div>
   );
 }
--- app/components/link/index.tsx
+++ app/components/link/index.tsx
@@ -1,17 +1,31 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 type Props = {
   className?: string;
   text: string;
   href: string;
-  isMenu?:boolean
+  isMenu?: boolean;
 };
 
-function Links({text,className="",href,isMenu=false}: Props) {
+function Links({ text, className = "", href, isMenu = false }: Props) {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const targetId = href.replace(/.*\#/, "");
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <Link href={href} className={`block text-base text-gray-600 hover:text-gray-900 ${!isMenu&&"underline"} ${className}`} >{text}</Link>
-  )
+    <Link
+      onClick={handleClick}
+      href={href}
+      className={`block text-base text-gray-600 hover:text-gray-900 ${
+        !isMenu && "underline"
+      } ${className}`}
+    >
+      {text}
+    </Link>
+  );
 }
 
-export default Links
+export default Links;
